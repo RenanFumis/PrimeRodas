a55c5e58d345796dc5c3df7bd2bd0cf30c0f6104
RenanFumis <renan-oliveira19@hotmail.com>
2024-07-13 16:50:45 -0300

    criamos uma nova lasse cchamada brand(marca) onde nela será colocado o nome das marcas e vinculado ao modelo do carro no site admin e no banco de dados como forestkey

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 # Aqui vamos importar os modelos que queremos que apareçam no site de administração Django
 
@@ -7,6 +7,10 @@ class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model',)
     
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
 # Aqui vamos registrar o modelo Car com a classe CarAdmin
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
