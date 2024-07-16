4b0dc14fadc01d56abf2b899a6d7c06c56bef04e
RenanFumis <renan-oliveira19@hotmail.com>
2024-07-16 20:19:33 -0300

    coloquei o arquivo CSS, criando uma pasta static e colocando a sintaxe correta para que o arquivo html pegue as informações do css e do views.py

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def cars_view(request):
-    return render(request, 'cars.html')
+    return render(
+        request,
+        'cars.html',
+        {
+            'cars': {
+                'Toyota': {
+                    'model': 'Corolla',
+                    'year': 2020,
+                    'price': 20000,
+                },
+            }
+        }
+        )
