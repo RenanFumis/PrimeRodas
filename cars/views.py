771d2221dd3c85a0729d6316fb638765a9c33806
RenanFumis <renan-oliveira19@hotmail.com>
2024-07-21 13:54:09 -0300

    colocamos o for django na pagina HTML para que percorra todo o conteudo do nosso banco de dados e apareça para o usuário estruturado

--- cars/views.py
+++ cars/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
+from cars.models import Car
 
 def cars_view(request):
+    car_main = Car.objects.get(pk=1)
+    cars = Car.objects.all()
+    
+
     return render(
         request,
         'cars.html',
         {
-            'cars': {
-                'Toyota': {
-                    'model': 'Corolla',
-                    'year': 2020,
-                    'price': 20000,
-                },
-            }
+            'cars': cars,
+            'car_main': car_main
         }
+            
         )
