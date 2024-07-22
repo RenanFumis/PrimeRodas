a0fd90eef10f28909b354602e4b7556f22a55c89
RenanFumis <renan-oliveira19@hotmail.com>
2024-07-21 23:49:43 -0300

    fiz alguns testes de filtros dentro do código utilizando o método .filter

--- cars/views.py
+++ cars/views.py
@@ -4,6 +4,8 @@ from cars.models import Car
 def cars_view(request):
     car_main = Car.objects.get(pk=1)
     cars = Car.objects.exclude(pk=car_main.pk)
+    filters = Car.objects.filter(model__contains='C')
+    print(filters)
     
 
     return render(
@@ -11,7 +13,8 @@ def cars_view(request):
         'cars.html',
         {
             'cars': cars,
-            'car_main': car_main
+            'car_main': car_main,
+            'filters': filters
         }
             
         )
