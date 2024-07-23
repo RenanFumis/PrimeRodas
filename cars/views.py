1c4a07f2639a692c2c82c3917a5c3b6c85af19af
RenanFumis <renan-oliveira19@hotmail.com>
2024-07-22 22:29:31 -0300

    coloquei um if que filtra dee acordo com a busca do usuário trocando a imagem do carro principal pelo carro buscado

--- cars/views.py
+++ cars/views.py
@@ -4,17 +4,21 @@ from cars.models import Car
 def cars_view(request):
     car_main = Car.objects.get(pk=1)
     cars = Car.objects.exclude(pk=car_main.pk)
-    filters = Car.objects.filter(model__contains='C')
-    print(filters)
-    
+    search = request.GET.get('search')
 
+    if search:
+        car_main = Car.objects.filter(model__contains=search).first()  # Substitui o car_main pelo primeiro resultado da busca
+        cars = Car.objects.exclude(pk=car_main.pk) if car_main else Car.objects.all()
+    else:
+        cars = Car.objects.exclude(pk=car_main.pk)
+    
     return render(
         request,
         'cars.html',
         {
             'cars': cars,
             'car_main': car_main,
-            'filters': filters
+            
         }
             
         )
