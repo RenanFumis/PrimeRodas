a77381e5123f01e28a784d985dc7215a01921a87
RenanFumis <renan-oliveira19@hotmail.com>
2024-07-23 23:43:30 -0300

    consegui fazer o tratamento do erro de quando uma pessoa faz uma busca por um modelo que não está no banco de dados. Para isso atualizei o views.py e coloquei uma condição de validação do dado passado na busca

--- cars/views.py
+++ cars/views.py
@@ -5,20 +5,24 @@ def cars_view(request):
     car_main = Car.objects.get(pk=1)
     cars = Car.objects.exclude(pk=car_main.pk)
     search = request.GET.get('search')
+    no_results_image = '/static/images/no_results.png'  # Tratamento de quando não há o modelo pesquisado
 
     if search:
-        car_main = Car.objects.filter(model__icontains=search).first()  #Substitui o carro princcipal pelo primeiro resultado da busca
-        cars = Car.objects.exclude(pk=car_main.pk) if car_main else Car.objects.all()
+        car_main = Car.objects.filter(model__icontains=search).first()  # Substitui o carro principal pelo primeiro resultado da busca
+        if car_main:
+            cars = Car.objects.exclude(pk=car_main.pk)
+        else:
+            cars = Car.objects.all()
+            car_main = None  # Define car_main como None se não houver o modelo buscado
     else:
-        cars = Car.objects.exclude(pk=car_main.pk).order_by('-value')#Ordena os carros pelo valor do Maior para o Menor
-    
+        cars = Car.objects.exclude(pk=car_main.pk).order_by('-value')  # Ordena os carros pelo valor do maior para o menor
+
     return render(
         request,
         'cars.html',
         {
             'cars': cars,
             'car_main': car_main,
-            
+            'no_results_image': no_results_image,
         }
-            
-        )
+    )
