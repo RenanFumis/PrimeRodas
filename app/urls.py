e411a81f0149fc3fccab4f47185c9daf5c35d551
RenanFumis <renan-oliveira19@hotmail.com>
2024-07-30 21:46:01 -0300

    melhoramos a lógica do forms utilizando o método forms.model nativo do django

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ from cars.views import cars_view, new_car_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', cars_view, name='cars_list'),
     path('cars/', cars_view, name='cars_list'),
     path('new_car/', new_car_view, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #aqui no static, estamos dizendo que se a url for para media, ele vai procurar no MEDIA_ROOT
--- cars/forms.py
+++ cars/forms.py
@@ -1,26 +1,32 @@
 from django import forms
 from cars.models import Brand, Car
 
-# Aqui estamos criando um formulário para o modelo Car
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
+# Aqui estamos criando um formulário para o modelo Car de forma manual
+# class CarForm(forms.Form):
+#     model = forms.CharField(max_length=200)
+#     brand = forms.ModelChoiceField(Brand.objects.all())
+#     factory_year = forms.IntegerField()
+#     model_year = forms.IntegerField()
+#     plate = forms.CharField(max_length=10)
+#     value = forms.FloatField()
+#     photo = forms.ImageField()
 
 
-    def save(self):
-        car = Car(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            value = self.cleaned_data['value'],
-            photo = self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
+#     def save(self):
+#         car = Car(
+#             model = self.cleaned_data['model'],
+#             brand = self.cleaned_data['brand'],
+#             factory_year = self.cleaned_data['factory_year'],
+#             model_year = self.cleaned_data['model_year'],
+#             plate = self.cleaned_data['plate'],
+#             value = self.cleaned_data['value'],
+#             photo = self.cleaned_data['photo'],
+#         )
+#         car.save()
+#         return car
+
+# Aqui estamos criando um formulário para o modelo Car de forma automática (mais usual)
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
