9c3e9db09346efe01b58481d86246ed6302cf056
RenanFumis <renan-oliveira19@hotmail.com>
2024-08-04 11:52:19 -0300

    fizemos a parte de cadastramento e validação de um usuários, com os tratamentos de erro de nome de usuário já existe e senha fraca

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view
+from accounts.views import register_view
 
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path('', cars_view, name='cars_list'),
     path('cars/', cars_view, name='cars_list'),
     path('new_car/', new_car_view, name='new_car'),
+    path('register/', register_view, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #aqui no static, estamos dizendo que se a url for para media, ele vai procurar no MEDIA_ROOT
