d0c2481adf06238c50d0a4b58dc906ff14c3e4d0
RenanFumis <renan-oliveira19@hotmail.com>
2024-08-09 19:15:05 -0300

    refatorei a class e utilizei uma call based view do django

--- cars/views.py
+++ cars/views.py
@@ -2,35 +2,44 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarForm
 from django.views import View
+from django.views.generic import ListView
 
-class CarsView(View):
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+    
+    def get_queryset(self):
+        search = self.request.GET.get('search')
+        
+        if search:
+            car_main = Car.objects.filter(model__icontains=search).first()
+            if car_main:
+                queryset = Car.objects.exclude(pk=car_main.pk)
+            else:
+                queryset = Car.objects.all()
+        else:
+            car_main = Car.objects.get(pk=1)
+            queryset = Car.objects.exclude(pk=car_main.pk).order_by('-value')
 
-    def get(self, request):
-        car_main = Car.objects.get(pk=1)
-        cars = Car.objects.exclude(pk=car_main.pk)
-        search = request.GET.get('search')
-        no_results_image = '/static/images/no_results.png'  # Tratamento de quando não há o modelo pesquisado
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        search = self.request.GET.get('search')
+        no_results_image = '/static/images/no_results.png'
 
         if search:
-            car_main = Car.objects.filter(model__icontains=search).first()  # Substitui o carro principal pelo primeiro resultado da busca
-            if car_main:
-                cars = Car.objects.exclude(pk=car_main.pk)
-            else:
-                cars = Car.objects.all()
-                car_main = None  # Define car_main como None se não houver o modelo buscado
+            car_main = Car.objects.filter(model__icontains=search).first()
         else:
-            cars = Car.objects.exclude(pk=car_main.pk).order_by('-value')  # Ordena os carros pelo valor do maior para o menor
+            car_main = Car.objects.get(pk=1)
 
-        return render(
-            request,
-            'cars.html',
-            {
-                'cars': cars,
-                'car_main': car_main,
-                'no_results_image': no_results_image,
-            }
-        )
-    
+        context['car_main'] = car_main
+        context['no_results_image'] = no_results_image
+
+        return context
+
 class NewCarView(View):
 
     def get(self, request):
