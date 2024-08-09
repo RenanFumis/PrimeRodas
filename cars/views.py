288321c7fd59fb6d24b66199aee0b4043a949d2b
RenanFumis <renan-oliveira19@hotmail.com>
2024-08-08 21:39:19 -0300

    refatorei a funçcão de new_cars_view para uma classe NewCarView onde tratamos melhor o if else

--- cars/views.py
+++ cars/views.py
@@ -3,10 +3,8 @@ from cars.models import Car
 from cars.forms import CarForm
 from django.views import View
 
-
-
 class CarsView(View):
-    
+
     def get(self, request):
         car_main = Car.objects.get(pk=1)
         cars = Car.objects.exclude(pk=car_main.pk)
@@ -32,23 +30,28 @@ class CarsView(View):
                 'no_results_image': no_results_image,
             }
         )
+    
+class NewCarView(View):
 
-
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-
-
-
-    else:
+    def get(self, request):
         new_car_form = CarForm()
-    return render(
-        request,
-        'new_car.html',
-        {
-            'new_car_form': new_car_form,
-        }
-        )
+        return render(
+            request,
+            'new_car.html',
+            {
+                'new_car_form': new_car_form,
+            }
+            )
+    
+    def post(self, request):
+            new_car_form = CarForm(request.POST, request.FILES)
+            if new_car_form.is_valid():
+                new_car_form.save()
+                return redirect('cars_list')
+            return render(
+                request,
+                'new_car.html',
+                {
+                    'new_car_form': new_car_form,
+                }
+                )
