8b2c42db8ba38d28c25eaa39eefe0360af45ca30
RenanFumis <renan-oliveira19@hotmail.com>
2024-08-08 21:08:31 -0300

    testamos a primeira substituição de uma função por uma Class basic views

--- cars/views.py
+++ cars/views.py
@@ -1,32 +1,38 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarForm
+from django.views import View
 
-def cars_view(request):
-    car_main = Car.objects.get(pk=1)
-    cars = Car.objects.exclude(pk=car_main.pk)
-    search = request.GET.get('search')
-    no_results_image = '/static/images/no_results.png'  # Tratamento de quando não há o modelo pesquisado
 
-    if search:
-        car_main = Car.objects.filter(model__icontains=search).first()  # Substitui o carro principal pelo primeiro resultado da busca
-        if car_main:
-            cars = Car.objects.exclude(pk=car_main.pk)
+
+class CarsView(View):
+    
+    def get(self, request):
+        car_main = Car.objects.get(pk=1)
+        cars = Car.objects.exclude(pk=car_main.pk)
+        search = request.GET.get('search')
+        no_results_image = '/static/images/no_results.png'  # Tratamento de quando não há o modelo pesquisado
+
+        if search:
+            car_main = Car.objects.filter(model__icontains=search).first()  # Substitui o carro principal pelo primeiro resultado da busca
+            if car_main:
+                cars = Car.objects.exclude(pk=car_main.pk)
+            else:
+                cars = Car.objects.all()
+                car_main = None  # Define car_main como None se não houver o modelo buscado
         else:
-            cars = Car.objects.all()
-            car_main = None  # Define car_main como None se não houver o modelo buscado
-    else:
-        cars = Car.objects.exclude(pk=car_main.pk).order_by('-value')  # Ordena os carros pelo valor do maior para o menor
+            cars = Car.objects.exclude(pk=car_main.pk).order_by('-value')  # Ordena os carros pelo valor do maior para o menor
 
-    return render(
-        request,
-        'cars.html',
-        {
-            'cars': cars,
-            'car_main': car_main,
-            'no_results_image': no_results_image,
-        }
-    )
+        return render(
+            request,
+            'cars.html',
+            {
+                'cars': cars,
+                'car_main': car_main,
+                'no_results_image': no_results_image,
+            }
+        )
+
 
 def new_car_view(request):
     if request.method == 'POST':
